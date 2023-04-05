0534445d3bbb11ea39856ca6afdfff193648d4a2
Sant Surat <>
2023-04-05 14:00:02 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,18 @@
+var x=7;
+function getName() {
+  console.log("Hello programmer")
+  console.log(x);
+  //return 0;
+}
+
+const getNaam = () => {
+  console.log("Hello programmer")
+  console.log(x);
+  return "abc";
+}
+var abc = getNaam();
+console.log(abc);
+
+
+
+
