f60f5d2fb29a5e63b838e7592c4d5a715eb0cdf7
@Sαɳƚ Sυɾαƚ <65957472+santsurat@users.noreply.github.com>
2023-04-06 08:59:31 +0000

    I changed varible declereation

--- index.js
+++ index.js
@@ -1,12 +1,15 @@
-var x=7;
+
 function getName() {
   console.log("Hello programmer")
   console.log(x);
-  //return 0;
+  return 0;
 }
+getName();
+
+var x=7;
 
 const getNaam = () => {
-  console.log("Hello programmer")
+  console.log("Hello world")
   console.log(x);
   return "abc";
 }
